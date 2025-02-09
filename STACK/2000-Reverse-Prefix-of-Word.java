30db0981bd8da9c7b4ea9996669130b021136dd8
Lokeshwar R <lokeshwarrajan4@gmail.com>
2025-02-09 19:21:47 +0530

    [2000] [Time Beats: 100.00%] [Memory Beats: 79.86%] - LeetPush

--- STACK/2000-Reverse-Prefix-of-Word.java
+++ STACK/2000-Reverse-Prefix-of-Word.java
@@ -1,14 +1,11 @@
 class Solution {
     public String reversePrefix(String word, char ch) {
-        int n = word.length();
-        int c= word.indexOf(ch);
-        String output = "";
-        for(int i=c;i>=0;i--){
-            output+=word.charAt(i);
-        }
-        for(int i=c+1;i<n;i++){
-            output+=word.charAt(i);
-        }
-        return output;
+        StringBuilder sb = new StringBuilder();
+        int n = word.indexOf(ch);
+        String output = word.substring(0,n+1);
+        sb.append(output);
+        sb.reverse();
+        sb.append(word.substring(n+1));
+        return sb.toString();
     }
 }
